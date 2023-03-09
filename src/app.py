04f11bbd32eb90f38a1b1cf85f4f93fe0b9d404b
José Inácio Castelo Branco <zeinaciocb@gmail.com>
2023-03-09 19:18:11 +0000

    project finished. 2 new endpoints

--- src/app.py
+++ src/app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 #When the application runs it will take over your command line, you will not be able to type on it anymore because a server application (like flask) never stops running, it keeps waiting for "requests" forever.
 #What does this means? 
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
 
 @app.route('/todos', methods=['GET'])
-def hello_world():
-    return "<h1>Hello!</h1>" 
+def getTodos():
+    todosJson = jsonify(todos)
+    return todosJson
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    newTodo = request.json
+    print("Incoming todo: ", newTodo)
+    todos.append(newTodo)
+    todos_json = jsonify(todos)
+    return todos_json
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("Deleting the following todo: ", todos[position])
+    todos.pop(position)
+    todos_json = jsonify(todos)
+    return todos_json
 
 
 # These two lines should always be at the end of your app.py file.
